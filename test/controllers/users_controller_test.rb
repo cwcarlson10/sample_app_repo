a04db94f88503e3b0ed3cdcaef7e92b326af167e
Chris Carlson <cwcarlson10@gmail.com>
2014-12-10 10:30:40 -0700

    added some testing

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,4 +6,13 @@ class UsersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should display errors" do
+  	user_data = {name: 'Some Name', email: 'email@examplecom', password: 'foobar', password_confirmation: 'foobar' }
+  	post :create, user: user_data
+  	assert_select 'div#error_explination'
+  	assert_select 'div#error_explination' do
+  		assert_select 'li', { count: 1 }
+  	end
+  end
+
 end
